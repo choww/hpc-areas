9121509aa46253744552758abbd53b6ba1f099ae
Car <c_chow568@yahoo.ca>
2015-02-14 12:07:56 -0800

    Fixed volume calculations, adjusted layout of ColQueryDialog and added scrolling to it.

--- areas-v1.py
+++ areas-v1.py
@@ -1,7 +1,7 @@
 # !/usr/bin/python
 # -*- coding: utf-8 -*-
 import os, sys
-import wx
+import wx, wx.lib.scrolledpanel
 from xlrd import open_workbook
 from xlwt import Workbook
 from xlutils.copy import copy
@@ -23,14 +23,15 @@ class Count(wx.Frame):
         # EXCEL VARS
         self.orig_xls= ""
         self.wkbook = ""
-        self.wksheet = ""
+        self.wksheet = 0
+        self.old_sheet = ""
         self.new_book = ""
         self.new_sheet = 0
         self.wanted_rows = 0
         self.wanted_cols = 0
 
         # CALCULATION VARS
-        self.pixels = 0     # input pixels per micron
+        self.pixels = 0
 
         # APP BODY
 
@@ -85,18 +86,19 @@ class Count(wx.Frame):
         self.wksheet = sheets.GetSelection()
         sheets.Destroy()
 
-        return self.wkbook, self.wksheet
+        self.old_sheet = self.wkbook.sheet_by_index(self.wksheet)
+        self.wanted_rows = self.old_sheet.nrows
+
+        return self.wkbook, self.wksheet, self.old_sheet, self.wanted_rows
 
     def copy_xls(self):
         """
         Create a copy of the original excel file. 
         """
         self.new_book = copy(self.wkbook)
-        new_sheet = self.new_book.get_sheet(self.wksheet)
-        new_sheet.name = 'CALCULATED DATA'
-        self.new_sheet = self.wkbook.sheet_by_index(self.wksheet)
-        self.wanted_rows = self.new_sheet.nrows
-        return self.new_book, self.new_sheet, self.wanted_rows
+        self.new_sheet = self.new_book.get_sheet(self.wksheet)
+        self.new_sheet.name = 'CALCULATED DATA'
+        return self.new_book, self.new_sheet
         
     def start(self, event):
         self.copy_xls()
@@ -105,9 +107,11 @@ class Count(wx.Frame):
         self.dg_volume()
         
         self.wkbook.release_resources()
-        self.new_book.save('areas-02932075348902.xls')  # TEMPORARY--will allow user to save their own sheet. 
+        # TEMPORARY--will allow user to name their own file when they save.
+        self.new_book.save('areas-02932075348902.xls') 
 
     ##### PROGRAM START #####
+
 
     def col_prompt(self):
         """
@@ -115,29 +119,36 @@ class Count(wx.Frame):
         """
 
         choose = ColQueryDialog(None, title="Choose columns")
+        choose.EnableLayoutAdaptation(True)
         choose.ShowModal()
         self.wanted_cols = choose.GetValues()
         choose.Destroy()
+        
+        print self.wanted_cols
+        return self.wanted_cols
 
  
     def multiply_by_10(self):
         """
-        Cell counts - multiply all values by 10.
+        BrdU counts - multiply all values by 10.
         """
-        
+
         for row in range(1, self.wanted_rows):
             for col in range(len(self.wanted_cols['counts'])):
-                self.new_sheet.write(row, self.wanted_cols['counts'][col], sheet.cell(row, self.wanted_cols['counts'][col]).value * 10)               
+                self.new_sheet.write(row, self.wanted_cols['counts'][col],
+                                     self.old_sheet.cell(row, self.wanted_cols['counts'][col]).value * 10)               
 
     def dg_volume(self):
         """
-        Calculate dentate gyrus & hilar volume for each brain. 
+        Calculate DG & hilar volume for each brain. 
         """
         px = (self.pixels.GetValue()).encode('utf-8')
-        calc = (1/int(px))**2 * 2 * 0.4
+        calc = (1/float(px))**2 * 2 * 0.4
+        print calc
         for row in range(1, self.wanted_rows):
             for col in range(len(self.wanted_cols['areas'])):
-                self.new_sheet.write(row, self.wanted_cols['areas'][col], sheet.cell(row, self.wanted_cols['areas'][col]).value * calc)
+                self.new_sheet.write(row, self.wanted_cols['areas'][col],
+                                     self.old_sheet.cell(row, self.wanted_cols['areas'][col]).value * calc)
 
     def cell_density(self):
         #calculate cell density for each brain. counts/mm3
@@ -150,16 +161,20 @@ class ColQueryDialog(wx.Dialog):
     def __init__(self, *args, **kwargs):
         wx.Dialog.__init__(self, *args, **kwargs)
 
-        self.SetSize((300,600))
+        self.SetSize((300,500))
         self.Center()
-        pnl = wx.Panel(self)
+        pnl = wx.lib.scrolledpanel.ScrolledPanel(self)
 
-        instruction = "Select a column [e.g. A, B, etc] for each parameter."
+        pnl.SetupScrolling(scroll_x=False)
+
+        instruction = "Select a column [e.g. A, B, etc] for each parameter.\n" \
+                      "Leave blank if certain parameters are not needed." 
         t_instruct = wx.StaticText(pnl, label=instruction)
 
         sizer = wx.BoxSizer(wx.VERTICAL)
-        sizer.Add(t_instruct, 0, wx.ALL|wx.CENTER, 5)
+        sizer.Add(t_instruct, 0, wx.ALL|wx.CENTER, 10)
 
+        t_count = wx.StaticText(pnl, label="COUNTS")
         t_ddg = wx.StaticText(pnl, label="Dorsal dentate")
         self.ddg = wx.TextCtrl(pnl)
         t_vdg = wx.StaticText(pnl, label="Ventral dentate")
@@ -169,32 +184,47 @@ class ColQueryDialog(wx.Dialog):
         t_vhil = wx.StaticText(pnl, label="Ventral hilus")
         self.vhil = wx.TextCtrl(pnl)
 
-        t_ddgarea = wx.StaticText(pnl, label="Dorsal DG areas")
+        t_totdg = wx.StaticText(pnl, label="Total DG")
+        self.totdg = wx.TextCtrl(pnl)
+        t_tothil = wx.StaticText(pnl, label="Total hilus")
+        self.tothil = wx.TextCtrl(pnl)
+
+        t_area = wx.StaticText(pnl, label="AREAS")
+        t_ddgarea = wx.StaticText(pnl, label="Dorsal dentate areas")
         self.ddgarea = wx.TextCtrl(pnl)
-        t_vdgarea = wx.StaticText(pnl, label="Ventral DG areas")
+        t_vdgarea = wx.StaticText(pnl, label="Ventral dentate areas")
         self.vdgarea = wx.TextCtrl(pnl)
         t_dhilarea = wx.StaticText(pnl, label="Dorsal hilus areas")
         self.dhilarea = wx.TextCtrl(pnl)
         t_vhilarea = wx.StaticText(pnl, label="Ventral hilus areas")
         self.vhilarea = wx.TextCtrl(pnl)
 
-        sizer.Add(t_ddg)
-        sizer.Add(self.ddg)
-        sizer.Add(t_vdg)
-        sizer.Add(self.vdg)
-        sizer.Add(t_dhil)
-        sizer.Add(self.dhil)
-        sizer.Add(t_vhil)
-        sizer.Add(self.vhil)
+        sizer.Add(t_count, 0, wx.ALL, 10)
+        sizer.Add(t_ddg, 0, wx.LEFT, 5)
+        sizer.Add(self.ddg, 0, wx.LEFT, 5)
+        sizer.Add(t_vdg, 0, wx.LEFT, 5)
+        sizer.Add(self.vdg, 0, wx.LEFT, 5)
+        sizer.Add(t_dhil, 0, wx.LEFT, 5)
+        sizer.Add(self.dhil, 0, wx.LEFT, 5)
+        sizer.Add(t_vhil, 0, wx.LEFT, 5)
+        sizer.Add(self.vhil, 0, wx.LEFT, 5)
 
-        sizer.Add(t_ddgarea)
-        sizer.Add(self.ddgarea)
-        sizer.Add(t_vdgarea)
-        sizer.Add(self.vdgarea)
-        sizer.Add(t_dhilarea)
-        sizer.Add(self.dhilarea)
-        sizer.Add(t_vhilarea)
-        sizer.Add(self.vhilarea)
+        sizer.AddSpacer(20)
+        sizer.Add(t_totdg, 0, wx.LEFT, 5)
+        sizer.Add(self.totdg, 0, wx.LEFT, 5)
+        sizer.Add(t_tothil, 0, wx.LEFT, 5)
+        sizer.Add(self.tothil, 0, wx.LEFT, 5)
+        sizer.AddSpacer(20)
+
+        sizer.Add(t_area, 0, wx.ALL, 10)
+        sizer.Add(t_ddgarea, 0, wx.LEFT, 5)
+        sizer.Add(self.ddgarea, 0, wx.LEFT, 5)
+        sizer.Add(t_vdgarea, 0, wx.LEFT, 5)
+        sizer.Add(self.vdgarea, 0, wx.LEFT, 5)
+        sizer.Add(t_dhilarea, 0, wx.LEFT, 5)
+        sizer.Add(self.dhilarea, 0, wx.LEFT, 5)
+        sizer.Add(t_vhilarea, 0, wx.LEFT, 5)
+        sizer.Add(self.vhilarea, 0, wx.LEFT, 5)
 
         b_submit = wx.Button(pnl, wx.ID_OK, label="Submit")
         b_submit.SetDefault()
@@ -210,25 +240,31 @@ class ColQueryDialog(wx.Dialog):
         pnl.Layout()
 
     def GetValues(self):
-        wanted_cols = {}
-
+        wanted_cols = {} 
         wanted_cols['counts'] = []
         wanted_cols['counts'].append(self.ddg.GetValue())
         wanted_cols['counts'].append(self.vdg.GetValue())
         wanted_cols['counts'].append(self.dhil.GetValue())
         wanted_cols['counts'].append(self.vhil.GetValue())
 
+        wanted_cols['counts'].append(self.totdg.GetValue())
+        wanted_cols['counts'].append(self.tothil.GetValue())
+
         wanted_cols['areas'] = []
         wanted_cols['areas'].append(self.ddgarea.GetValue())
         wanted_cols['areas'].append(self.vdgarea.GetValue())
+        wanted_cols['areas'].append(self.dhilarea.GetValue())
         wanted_cols['areas'].append(self.vhilarea.GetValue())
-        wanted_cols['areas'].append(self.vhilarea.GetValue())
+
+        print wanted_cols
         
         letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-        for key in wanted_cols: 
+        for key in wanted_cols:
             for c in range(len(wanted_cols[key])):
                 wanted_cols[key][c] = wanted_cols[key][c].encode('utf-8').upper()
-                if len(wanted_cols[key][c]) == 1 or not wanted_cols[key][c] == None:
+                if wanted_cols[key][c] == "":
+                    del wanted_cols[key][c]
+                elif len(wanted_cols[key][c]) == 1:
                     wanted_cols[key][c] = letters.find(wanted_cols[key][c])
                 else:
                     a = 26
@@ -236,7 +272,6 @@ class ColQueryDialog(wx.Dialog):
                     for l in range(1,len(wanted_cols[key][c])):
                         b += letters.find(wanted_cols[key][c][1])
                     wanted_cols[key][c] = (letters.find(wanted_cols[key][c][0]) + 1) * a + b
-        print wanted_cols
         return wanted_cols
 
 def runfile():
